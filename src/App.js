cd6557a9b848c908cdbffc1150f8c7237df71fbe
abhishek raj goswami <goswamiabhishekraj@gmail.com>
2023-01-29 12:23:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Linkedin Clone</h1>
+      {/*Header*/}
+      <Header/>
+      {/*App Body*/}
+      {/*sidebar*/}
+      {/*feed*/}
+      {/*widgets*/}
+    </div>
+  );
+}
+
+export default App;
